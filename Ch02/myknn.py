4549305955b8f9d1f31bd482c00a2d8a9ea4994b
Yi-Kun Yang <ykyang@gmail.com>
2017-09-07 11:28:35 -0500

    Learn 2.1: file2matrix
    Followed the version from github not book

--- Ch02/myknn.py
+++ Ch02/myknn.py
@@ -20,6 +20,8 @@ Output:     the most popular class label
 
 @author: pbharrin
 '''
+
+
 def classify0(inx, dataset, labels, k):
     dataset_size = dataset.shape[0]
     diff_mat = np.tile(inx, (dataset_size, 1)) - dataset
@@ -36,9 +38,40 @@ def classify0(inx, dataset, labels, k):
 
     return sorted_class_count[0][0]
 
+
+def file2matrix(filename):
+    love_dictionary = {'largeDoses': 3, 'smallDoses': 2, 'didntLike': 1}
+
+    fr = open(filename)
+    line_count = len(fr.readlines())
+
+    return_matrix = np.zeros((line_count, 3))
+    class_label_vector = []
+
+    fr = open(filename)
+    index = 0
+    for line in fr.readlines():
+        line = line.strip()
+        tokens = line.split('\t')
+        return_matrix[index,:] = tokens[0:3]
+        label = tokens[-1]
+        if label.isdigit():
+            class_label_vector.append(int(label))
+        else:
+            class_label_vector.append(love_dictionary.get(label))
+
+        index += 1
+
+    return return_matrix, class_label_vector
+
+
 if __name__ == "__main__":
     inx = [0,0]
     group, labels = create_dataset()
     val = classify0(inx, group, labels, 3)
     print(val)
+
+    dating_data_matrix, dating_label = file2matrix('datingTestSet.txt')
+    print(dating_data_matrix)
+    print(dating_label)
 
